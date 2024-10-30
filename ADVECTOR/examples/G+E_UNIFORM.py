d27a9e1633d9dd26717e0942430ca140dcd7afb2
Mattia <m.romero@theoceancleanup.com>
2024-10-30 09:05:56 +0000

    G+E Uniform 1993-2024

--- ADVECTOR/examples/G+E_UNIFORM.py
+++ ADVECTOR/examples/G+E_UNIFORM.py
@@ -17,24 +17,23 @@ from datetime import datetime, timedelta
 from pathlib import Path
 
 
-ADVECTION_START = datetime(2020, 1, 1)
-ADVECTION_END = datetime(2023, 12, 31)
+ADVECTION_START = datetime(1992, 12, 31)
+ADVECTION_END = datetime(2024, 1, 1)
 WINDAGE_COEFF = 0
-source_file_path = r"Z:\ADVECTOR\sources\uniform\T1-500_RD2020.nc"
-OUTPUTFILE_PATH = f"Y:\PROJECTS\TRAPs\ADVECTOR\outputs"
-OUTPUTFILE_PATH = f"Y:\PROJECTS\TRAPs\ADVECTOR\outputs\\advector_v1_2d_globcurrent_uniform_GPGP_1x_wind_{WINDAGE_COEFF}_{ADVECTION_START.year}_{ADVECTION_END.year}"
+source_file_path = r"I:\ADVECTOR\sources\uniform\T0-8km_NPO_RD2020_landmask.nc"
+OUTPUTFILE_PATH = f"R:\PROJECTS\TRAPs\ADVECTOR\outputs"
+OUTPUTFILE_PATH = f"R:\PROJECTS\TRAPs\ADVECTOR\outputs\\advector_v1_2d_globcurrent_T0-8km_NPO_RD2020_landmask_1x_wind_{WINDAGE_COEFF}_{ADVECTION_START.year}_{ADVECTION_END.year}"
 water_varname_map = {"uo": "U", "vo": "V", "longitude": "lon", "latitude": "lat"}
 
 if __name__ == "__main__":
 
-    pth = Path(OUTPUTFILE_PATH)
-    if not pth.exists():
-        pth.mkdir()
+    # pth = Path(OUTPUTFILE_PATH)
+    # if not pth.exists():
+       # pth.mkdir()
 
     # download surface currents 
     download_path = r"Y:\PROJECTS\TRAPs\ADVECTOR\inputs"
-    download_and_process_currents(download_path, ADVECTION_START, ADVECTION_END)
-    download_and_process_currents(download_path, ADVECTION_START, ADVECTION_END)
+    download_and_process_currents(download_path, ADVECTION_START + timedelta(days=1), ADVECTION_END - timedelta(days=1))
     GLOBCURRENT_U_PATH = r"Y:\PROJECTS\TRAPs\ADVECTOR\inputs\raw\uo_*"
     GLOBCURRENT_V_PATH = r"Y:\PROJECTS\TRAPs\ADVECTOR\inputs\raw\vo_*"
 
@@ -50,7 +49,7 @@ if __name__ == "__main__":
         timestep=timedelta(hours=1),
         num_timesteps=24 * (ADVECTION_END - ADVECTION_START).days,
         save_period=24,
-        memory_utilization=0.2,  # decrease if RAM overloaded.  Can be close to 1 on dedicated compute device (e.g. GPU)
+        memory_utilization=0.4,  # decrease if RAM overloaded.  Can be close to 1 on dedicated compute device (e.g. GPU)
         opencl_device=None,
         show_progress_bar= True,
     )
